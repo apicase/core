83c5e4982c9592db28a399778294f8a6382670ca
Anton Kosykh <kelin2025@yandex.ru>
2017-10-05 02:21:07 +0300

    Added hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import xhrAdapter from 'adapters/xhr'
 import fetchAdapter from 'adapters/fetch'
-import { map, pipe, merge } from 'ramda'
+import { map, tap, pipe, clone, merge } from 'ramda'
+import { pipeM, composeHooks, normalizeHooks } from 'utils'
 
 export const Apicase = {
   // Options
@@ -17,12 +18,20 @@ export const Apicase = {
     this.adapters[name] = adapter
   },
   // Make API call
-  call ({ adapter = this.options.defaultAdapter, ...options }) {
+  async call ({
+    adapter = this.options.defaultAdapter,
+    hooks = {},
+    ...options
+  } = {}) {
+    const opts = clone(options)
+    const h = pipe(normalizeHooks, map(h => composeHooks(...h)))(hooks)
+    await h.before(opts)
     return new Promise((resolve, reject) => {
       this.adapters[adapter]({
         options,
-        done: resolve,
-        fail: reject
+        done: pipeM(h.success, resolve),
+        fail: pipeM(h.error, reject),
+        another: (name, data, fail = false) => pipeM(h[name], ...(fail ? [reject] : []))(data)
       })
     })
   },
@@ -41,6 +50,16 @@ export const Apicase = {
     }
   }
 }
+
+Apicase.call({
+  url: '/root/:id',
+  params: { id: 1 },
+  hooks: {
+    before: () => {},
+    success: (ctx, next) => console.log(ctx) || next(),
+    error: (ctx, next) => console.log(ctx) || next()
+  }
+}).then(console.log).catch(console.log)
 
 if (process.env.NODE_ENV === 'development') {
   console.log('[DEV] Apicase-core is available in browser console via Apicase global variable')
--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,40 @@
-import { is, map, set, join, keys, pipe, when, assoc, curry, concat, reduce, isEmpty, toPairs, identity, complement } from 'ramda'
+import { is, of, map, set, join, keys, pipe, when, assoc, curry, merge, concat, reduce, compose, isEmpty, toPairs, identity, complement } from 'ramda'
 
 const isNotEmpty = complement(isEmpty)
 const encodeParts = map(map(encodeURIComponent))
 const joinParts = pipe(map(join('=')), join('&'))
 const addQuestionSign = when(isNotEmpty, concat('?'))
 
-// TODO: Looking for point-free variants
-
 // Convert json to QueryString
 export const jsonToQueryString = pipe(toPairs, encodeParts, joinParts, addQuestionSign)
 
 // Like R.over but use object in callback instead of prop
+// TODO: Looking for point-free variant
 export const overAll = curry((lens, cb, data) => set(lens, cb(data), data))
 
 // Change prop name and return object
+// TODO: Looking for point-free variant
 export const rename = curry((keyFrom, keyTo, obj) =>
   reduce((acc, key) => assoc(key === keyFrom ? keyTo : key, obj[key], acc), {}, keys(obj))
 )
+
+// Helper for adapters to evaluate dynamically declared headers
+export const evaluateHeaders = when(is(Function), h => h())
+
+// Normalize hooks
+export const normalizeHooks = pipe(
+  merge({ before: [], success: [], error: [], finished: [] }),
+  map(when(is(Function), of))
+)
+
+// Compose async functions
+// Based on koa-compose idea (https://github.com/koajs/compose)
+export const composeHooks = (...fns) => ctx =>
+  fns.length === 0
+    ? Promise.resolve(ctx)
+    : Promise.resolve(fns[0](ctx, () =>
+      composeHooks(...fns.slice(1))(ctx)
+    ))
 
 // Like R.pipeP but work with non-Promises too
 export const pipeM = (...fns) => arg => {
@@ -26,8 +44,3 @@ export const pipeM = (...fns) => arg => {
   const isPromise = typeof res === 'object' && 'then' in res
   return isPromise ? res.then(i => next(i)) : next(res)
 }
-
-// Helper for adapters to evaluate dynamically declared headers
-export const evaluateHeaders = when(is(Function), h => h())
-
-// Compose async
