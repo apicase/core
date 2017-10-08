b7ae9322b20303b8aad63c490b8bb68d4dff75cf
Anton Kosykh <kelin2025@yandex.ru>
2017-10-08 03:54:30 +0300

    Fixed options and hooks merging. Added flow types

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import xhrAdapter from 'adapters/xhr'
-import fetchAdapter from 'adapters/fetch'
-import { map, omit, pipe, clone, merge, concat, mergeWith } from 'ramda'
-import { pipeM, composeHooks, normalizeHooks } from 'utils'
+// @flow
+import * as Types from './types'
+import xhrAdapter from './adapters/xhr'
+import fetchAdapter from './adapters/fetch'
+import { pipeM, mergeHooks, mergeOptions, mapComposeHooks } from './utils'
 
-export const Apicase = {
+export const Apicase: Types.Apicase = {
+
   // Base for calls
-  base: { },
+  base: {
+    query: {},
+    hooks: { }
+  },
+
   // Options
   options: {
     defaultAdapter: 'fetch'
   },
+
   // Built-in adapters for fetch and XMLHttpRequest
   adapters: {
     fetch: fetchAdapter,
     xhr: xhrAdapter
   },
+
   // Use it to add a new adapter
   use (name, adapter) {
     this.adapters[name] = adapter
   },
+
   // Make API call
   // TODO: Make it a little simpler
   async call ({
@@ -26,53 +35,38 @@ export const Apicase = {
     hooks = {},
     ...options
   } = {}) {
-
-    const opts = merge(
-      omit('hooks', this.base),
-      clone(options),
-      { adapter }
-    )
-
-    const h = pipe(
-      normalizeHooks,
-      mergeWith(concat, normalizeHooks(this.base.hooks)),
-      map(h => composeHooks(...h))
-    )(hooks)
-
-    await h.before(opts)
-
+    const query = mergeOptions(this.base.query, options)
+    const h = mapComposeHooks(mergeHooks(this.base.hooks, hooks))
+    await h.before(query)
     return new Promise((resolve, reject) => {
       this.adapters[adapter]({
-        options,
+        options: query,
         done: pipeM(h.success, resolve),
         fail: pipeM(h.error, reject),
-        another: (name, data, fail = false) => pipeM(h[name], ...(fail ? [reject] : []))(data)
+        another: (name, data, fail = false) =>
+          pipeM(h[name], ...(fail ? [reject] : []))(data)
       })
     })
-
   },
+
   // Like Promise.all but iteratees over calls options
   all (queries) {
-    return Promise.all(queries.map(this.call))
+    return Promise.all(queries.map(q => this.call(q)))
   },
+
   // Make a service with prepared options
   // Options from call() and all() methods will be merged with service options
-  // TODO: Improve this method and realize it without hacks
-  of (base) {
-    const self = clone(this)
-    self.base = merge(self.base, base)
-    return self
+  of ({ hooks = {}, ...options }) {
+    return {
+      ...this,
+      base: {
+        query: mergeOptions(this.base.hooks, options),
+        hooks: mergeHooks(this.base.hooks, hooks)
+      }
+    }
   }
-}
 
-Apicase.of({ url: '/root/:id', hooks: { before: () => {} } }).call({
-  params: { id: 1 },
-  hooks: {
-    before: () => {},
-    success: (ctx, next) => console.log(ctx) || next(),
-    error: (ctx, next) => console.log(ctx) || next()
-  }
-}).then(console.log).catch(console.log)
+}
 
 if (process.env.NODE_ENV === 'development') {
   console.log('[DEV] Apicase-core is available in browser console via Apicase global variable')
