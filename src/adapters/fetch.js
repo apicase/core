a755fc22eed9107c50a16bd79b141d8a3073eeb8
Anton Kosykh <kelin2025@yandex.ru>
2017-10-08 03:55:03 +0300

    Little refactoring + added flow types

--- src/adapters/fetch.js
+++ src/adapters/fetch.js
@@ -1,22 +1,34 @@
+// @flow
+import type { Adapter } from '../types'
 import { compile } from 'path-to-regexp'
-import { pipeM, overAll, evaluateHeaders, jsonToQueryString } from 'utils'
-import { map, over, pipe, view, merge, concat, converge, lensProp } from 'ramda'
+import { pipeM, overAll, evaluateHeaders, jsonToQueryString } from '../utils'
+import { over, pipe, prop, merge, concat, converge, lensProp } from 'ramda'
 
-const [urlLens, queryLens, paramsLens, headersLens] = map(lensProp)(['url', 'query', 'params', 'headers'])
+type FetchAdapterQuery = {
+  url?: string,
+  parser?: 'json' | 'blob' | 'text' | 'formData' | 'arrayBuffer',
+  params?: { [key: string]: string | number | null },
+  query?: { [key: string]: string | number | null },
+  data?: Object | FormData | string | void
+}
 
-const [getUrl, getQuery, getParams] = map(view)([urlLens, queryLens, paramsLens])
-
-const [updateUrl, updateHeaders] = [overAll(urlLens), over(headersLens)]
+const urlLens = lensProp('url')
+const headersLens = lensProp('headers')
 
 // Shortcuts
 // TODO: Uncurry not working with path-to-regexp.compile. Need to test and fix
-const insertParams = converge((url, params) => compile(url)(params), [getUrl, getParams])
+const insertParams = converge(
+  (url, params) => compile(url)(params),
+  [prop('url'), prop('params')]
+)
 
-const insertQueryString = converge(concat, [getUrl, pipe(getQuery, jsonToQueryString)])
+const insertQueryString = converge(concat, [prop('url'), pipe(prop('query'), jsonToQueryString)])
 
 // IDEA: Add query options to done and fail callbacks
 const goFetch = (done, fail) => ({ url, parser, ...options}) =>
   fetch(url, options)
+    // I'm sure that Response has parser of needed type
+    // $FlowFixMe
     .then(r => r[parser]())
     .then(done)
     .catch(fail)
@@ -25,13 +37,14 @@ const goFetch = (done, fail) => ({ url, parser, ...options}) =>
 const setFetchDefaults = merge({ method: 'GET', body: undefined, headers: {}, parser: 'json', credentials: 'omit' })
 
 // Combine all the above
-export default function fetchAdapter ({ options, done, fail }) {
-  return pipeM(
-    // pickFetchParams,
+const fetchAdapter: Adapter<FetchAdapterQuery> = ({ options, done, fail }) => {
+  pipeM(
     setFetchDefaults,
-    updateUrl(insertParams),
-    updateUrl(insertQueryString),
-    updateHeaders(evaluateHeaders),
+    overAll(urlLens)(insertParams),
+    overAll(urlLens)(insertQueryString),
+    over(headersLens)(evaluateHeaders),
     goFetch(done, fail)
   )(options)
 }
+
+export default fetchAdapter
--- src/adapters/xhr.js
+++ src/adapters/xhr.js
@@ -1,24 +1,29 @@
-import { evaluateHeaders } from 'utils'
+// @flow
+import { type Adapter } from '../types'
+import { evaluateHeaders } from '../utils'
 import { merge, forEachObjIndexed } from 'ramda'
 
-export default function xhrAdapter ({ options, done, fail, another }) {
+type XHRAdapterQuery = {
+  url?: string,
+  params?: { [key: string]: string | number | null },
+  query?: { [key: string]: string | number | null },
+  data?: Object | FormData | string | void
+}
+
+const xhrAdapter: Adapter<XHRAdapterQuery> = ({ options, done, fail, another }) => {
   const opts = merge({ method: 'GET', body: undefined, headers: {} })(options)
   opts.headers = evaluateHeaders(opts.headers)
   const xhr = new XMLHttpRequest()
   xhr.open(opts.method, opts.url, true)
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState !== 4) {
-      return
-    }
-    if (xhr.status < 400) {
-      done(JSON.parse(xhr.responseText))
-    } else {
-      fail(xhr)
-    }
-  }
+  // $FlowFixMe
+  xhr.onload = (e) => done(JSON.parse(e.target.responseText))
+  xhr.onerror = e => fail(e)
   xhr.onprogress = e => another('progress', e)
+  xhr.onabort = e => another('aborted', e)
   forEachObjIndexed(opts.headers, (value, header) =>
     xhr.setRequestHeader(header, value)
   )
   xhr.send(opts.body)
 }
+
+export default xhrAdapter
