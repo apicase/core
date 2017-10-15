93e9d170ae425e5c487cf5c8b8c536ea7ab98d71
Kelin2025 <kelin2025@yandex.ru>
2017-10-15 03:16:22 +0300

    Minor changes in webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,34 +1,33 @@
 /* eslint-env node */
 require('extract-text-webpack-plugin')
 const webpack = require('webpack')
-const UglifyESPlugin = require('uglify-es-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const webpackSettings = require('./webpack.config')
 const CompressionPlugin = require("compression-webpack-plugin")
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const optimizingPlugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('production')
   }),
   new webpack.optimize.OccurrenceOrderPlugin,
-  new UglifyESPlugin({
+  new UglifyJSPlugin({
     sourceMap: false,
-    compress: {
-      warnings: false
-    },
-    output: {
-      beautify: false,
-      comments: false
+    uglifyOptions: {
+      compress: {
+        warnings: false
+      },
+      output: {
+        beautify: false,
+        comments: false
+      }
     }
   }),
   new CompressionPlugin({
     asset: "[path].gz[query]",
     algorithm: "gzip",
     test: /\.(js|html)$/,
-    threshold: 10240,
     minRatio: 0.8
   })
-  // new BundleAnalyzerPlugin
 ]
 
 webpackSettings.devtool = false
