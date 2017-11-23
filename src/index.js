9b01e7a5499a5d671222c2f9d1f56dc0fbc232b1
Kelin2025 <kelin2025@yandex.ru>
2017-11-23 13:36:44 +0300

    Added Apicase base methods

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import clone from 'clone'
+import NanoEvents from 'nanoevents'
+
+import mergeOptions from './mergeOptions'
+
+const Apicase = {
+  base: {
+    query: {},
+    hooks: {
+      before: [],
+      success: [],
+      error: [],
+      aborted: []
+    },
+    interceptors: {
+      before: [],
+      success: [],
+      error: [],
+      aborted: []
+    }
+  },
+
+  options: {
+    defaultAdapter: 'fetch',
+    adapters: {}
+  },
+
+  use (name, adapter) {
+    this.options.adapters[name] = adapter
+  },
+
+  async install (installer, options) {
+    this.bus.emit('preinstall', this)
+    await installer(this, options)
+    this.bus.emit('postinstall', this)
+  },
+
+  extend (installer, options) {
+    return clone(this).install(installer, options)
+  },
+
+  of ({ hooks, interceptors, ...query }) {
+    return {
+      ...this,
+      base: mergeOptions(this.base, {
+        query,
+        hooks,
+        interceptors
+      })
+    }
+  },
+
+  call ({ hooks, interceptors, ...query }) {
+    const o = mergeOptions(this.base, {
+        query,
+        hooks,
+        interceptors
+      })
+    console.log(o)
+    // TODO: Complete this
+  },
+
+  all (options) {
+    return Promise.all(options.map(q => this.call(q)))
+  },
+
+  on (event, callback) {
+    return this.bus.on(event, callback)
+  },
+
+  emit (event, ...data) {
+    this.bus.emit(event, ...data)
+  },
+
+  bus: new NanoEvents
+}
+
+export default Apicase
