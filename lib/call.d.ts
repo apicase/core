952ce89124a4e64c12d56dd8e4419defd7391268
Kelin2025 <kelin2025@yandex.ru>
2018-02-02 00:07:50 +0300

    Started flow/ts types for apicase call

---
+++ lib/call.d.ts
@@ -0,0 +1,24 @@
+function HookCallback<T>(payload: T): T
+
+function Hook<T>(opts: {
+  payload: T
+  next: HookCallback<T>
+  resolve?: HookCallback<T>
+  reject?: HookCallback<T>
+}): T
+
+interface HooksObject<T> {
+  [type: string]: Hook<T>[]
+}
+
+export default function apicase<T>(
+  adapter: {
+    callback: ({ payload: T, resolve: (any) => any, reject: (any) => any }) => any
+    convert?: (from: T) => T
+    merge?: (from: T, to: T) => T
+  },
+  req: T & {
+    meta?: { [key: string]: any }
+    hooks?: HooksObject<T>
+  }
+): T
