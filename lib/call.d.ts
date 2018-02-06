3179d7df9205722340560b7fd93add9cd787086a
Kelin2025 <kelin2025@yandex.ru>
2018-02-07 02:28:45 +0300

    Fixed typings. TS typing for .call() is temporary disabled because TS haven't spread (suddenly)

--- lib/call.d.ts
+++ lib/call.d.ts
@@ -1,24 +1,22 @@
-function HookCallback<T>(payload: T): T
+// function HookCallback<T>(payload: T): T
 
-function Hook<T>(opts: {
-  payload: T
-  next: HookCallback<T>
-  resolve?: HookCallback<T>
-  reject?: HookCallback<T>
-}): T
+// function Hook<T>(opts: {
+//   payload: T
+//   next: HookCallback<T>
+//   resolve?: HookCallback<T>
+//   reject?: HookCallback<T>
+// }): T
 
-interface HooksObject<T> {
-  [type: string]: Hook<T>[]
-}
+// interface HooksObject<T> {
+//   [type: string]: Hook<T>[]
+// }
 
-export default function apicase<T>(
-  adapter: {
-    callback: ({ payload: T, resolve: (any) => any, reject: (any) => any }) => any
-    convert?: (from: T) => T
-    merge?: (from: T, to: T) => T
-  },
-  req: T & {
-    meta?: { [key: string]: any }
-    hooks?: HooksObject<T>
-  }
-): T
+// export default function apicase<T>(
+//   adapter: {
+//     callback: ({ payload: T, resolve: (any) => any, reject: (any) => any }) => any
+//     convert?: (from: T) => T
+//     merge?: (from: T, to: T) => T
+//   }): ({T & {
+//     meta?: { [key: string]: any }
+//     hooks?: HooksObject<T>
+//   }) => T
