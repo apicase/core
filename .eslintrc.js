9fb35f7466bdc7af12c6bf8715ffe452453509b0
Anton Kosykh <a.kosyh@growfood.pro>
2018-02-20 17:04:00 +0300

    .eslintrc fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
 
   'rules': {
     /* It's better to have indent for call expr with multi-lines */
-    'indent': ['error', 2, { 'CallExpression': { 'arguments': 1 } }]
+    'indent': ['error', 2, { 'CallExpression': { 'arguments': 1 } }],
     /* Prettier doesn't support space before paren */
     'space-before-function-paren': 0
   }
