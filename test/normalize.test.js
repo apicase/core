d321d9adac266951979d7abd247e628d7ef8c73e
Kelin2025 <kelin2025@yandex.ru>
2018-02-23 02:09:58 +0300

    Added tests for normalizeOptions()

--- test/normalize.test.js
+++ test/normalize.test.js
@@ -4,17 +4,17 @@ describe('Hooks', () => {
   it('converts hooks to array of hooks', () => {
     const opts = {
       hooks: {
-        before() { },
-        resolve: [() => { }, () => { }]
+        before() {},
+        resolve: [() => {}, () => {}]
       }
     }
-    const h = normalizeOptions(opts).hooks
+    const h = normalizeOptions(null, opts).hooks
     expect(h.before).toEqual([opts.hooks.before])
     expect(h.resolve).toEqual([opts.hooks.resolve[0], opts.hooks.resolve[1]])
   })
 
   it('creates default hooks properties', () => {
-    const h = normalizeOptions({}).hooks
+    const h = normalizeOptions(null, {}).hooks
     expect(h).toEqual({
       before: [],
       resolve: [],
@@ -25,18 +25,18 @@ describe('Hooks', () => {
 
 describe('Meta', () => {
   it('leaves meta "as is"', () => {
-    expect(normalizeOptions({ meta: { a: 2 } }).meta).toEqual({ a: 2 })
+    expect(normalizeOptions(null, { meta: { a: 2 } }).meta).toEqual({ a: 2 })
   })
 
   it("creates empty object when it's not passed", () => {
-    expect(normalizeOptions({}).meta).toEqual({})
+    expect(normalizeOptions(null, {}).meta).toEqual({})
   })
 })
 
 describe('Payload', () => {
   it('passes all another properties to payload', () => {
     const opts = { a: 1, b: 2 }
-    const normalized = normalizeOptions(opts)
+    const normalized = normalizeOptions(null, opts)
     expect('a' in normalized).toBeFalsy()
     expect('b' in normalized).toBeFalsy()
     expect(normalized.payload).toEqual(opts)
@@ -45,7 +45,7 @@ describe('Payload', () => {
 
 describe('Applying normalize 2+ times', () => {
   it('adds _isNormalized flag', () => {
-    expect(normalizeOptions({})._isNormalized).toBeTruthy()
+    expect(normalizeOptions(null, {})._isNormalized).toBeTruthy()
   })
 
   it('does not normalize object that has already done', () => {
@@ -53,16 +53,19 @@ describe('Applying normalize 2+ times', () => {
       a: 1,
       b: 2,
       hooks: {
-        before: () => { }
+        before: () => {}
       }
     }
-    const res = normalizeOptions(normalizeOptions(opts))
+    const res = normalizeOptions(null, normalizeOptions(null, opts))
     expect(res).toEqual({
       _isNormalized: true,
+      adapter: null,
       payload: { a: 1, b: 2 },
       meta: {},
       hooks: {
-        before: [opts.hooks.before]
+        before: [opts.hooks.before],
+        resolve: [],
+        reject: []
       }
     })
   })
